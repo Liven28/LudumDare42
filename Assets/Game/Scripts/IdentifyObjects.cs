22f30a224ae65b664d8f7899cd2f8ce69018098a
Liven Herbo <livenvh@yahoo.fr>
2018-08-12 12:37:18 +0200

    Nv Visuals

--- Assets/Game/Scripts/IdentifyObjects.cs
+++ Assets/Game/Scripts/IdentifyObjects.cs
@@ -13,64 +13,16 @@ public class IdentifyObjects : MonoBehaviour
 
     private IdentifyObjects scrIdentifyObjects;
 
+    [SerializeField] private bool ActiveScript;
 	
 
     private void OnCollisionEnter2D(Collision2D collision2D)
     {
+        if (ActiveScript == true)
+        { 
         tagObject = collision2D.gameObject.tag;
-        if (tagObject == "Wall01")
-        {
-            if (connectedToWall02 == false)
+            if (tagObject == "Wall01")
             {
-                connectedToWall01 = true;
-            }
-            else
-            {
-                connectedToWall01 = false;
-                connectedToWall02 = false;
-            }
-        }
-        else if (tagObject == "Wall02")
-        {
-            if (connectedToWall01 == false)
-            {
-                connectedToWall02 = true;
-            }
-            else
-            {
-                connectedToWall01 = false;
-                connectedToWall02 = false;
-            }
-        }
-        else if (tagObject == "Wall03")
-        {
-            if (connectedToWall04 == false)
-            {
-                connectedToWall03 = true;
-            }
-            else
-            {
-                connectedToWall03 = false;
-                connectedToWall04 = false;
-            }
-        }
-        else if (tagObject == "Wall04")
-        {
-            if (connectedToWall03 == false)
-            {
-                connectedToWall04 = true;
-            }
-            else
-            {
-                connectedToWall03 = false;
-                connectedToWall04 = false;
-            }
-        }
-        else if (tagObject == "Player" || tagObject == "Trash")
-        {
-            scrIdentifyObjects = collision2D.gameObject.GetComponent<IdentifyObjects>();
-
-            if (scrIdentifyObjects.connectedToWall01)
                 if (connectedToWall02 == false)
                 {
                     connectedToWall01 = true;
@@ -80,7 +32,9 @@ public class IdentifyObjects : MonoBehaviour
                     connectedToWall01 = false;
                     connectedToWall02 = false;
                 }
-            if (scrIdentifyObjects.connectedToWall02)
+            }
+            else if (tagObject == "Wall02")
+            {
                 if (connectedToWall01 == false)
                 {
                     connectedToWall02 = true;
@@ -90,7 +44,9 @@ public class IdentifyObjects : MonoBehaviour
                     connectedToWall01 = false;
                     connectedToWall02 = false;
                 }
-            if (scrIdentifyObjects.connectedToWall03)
+            }
+            else if (tagObject == "Wall03")
+            {
                 if (connectedToWall04 == false)
                 {
                     connectedToWall03 = true;
@@ -100,7 +56,9 @@ public class IdentifyObjects : MonoBehaviour
                     connectedToWall03 = false;
                     connectedToWall04 = false;
                 }
-            if (scrIdentifyObjects.connectedToWall04)
+            }
+            else if (tagObject == "Wall04")
+            {
                 if (connectedToWall03 == false)
                 {
                     connectedToWall04 = true;
@@ -110,33 +68,82 @@ public class IdentifyObjects : MonoBehaviour
                     connectedToWall03 = false;
                     connectedToWall04 = false;
                 }
+            }
+            else if (tagObject == "Player" || tagObject == "Trash")
+            {
+                scrIdentifyObjects = collision2D.gameObject.GetComponent<IdentifyObjects>();
+
+                if (scrIdentifyObjects.connectedToWall01)
+                    if (connectedToWall02 == false)
+                    {
+                        connectedToWall01 = true;
+                    }
+                    else
+                    {
+                        connectedToWall01 = false;
+                        connectedToWall02 = false;
+                    }
+                if (scrIdentifyObjects.connectedToWall02)
+                    if (connectedToWall01 == false)
+                    {
+                        connectedToWall02 = true;
+                    }
+                    else
+                    {
+                        connectedToWall01 = false;
+                        connectedToWall02 = false;
+                    }
+                if (scrIdentifyObjects.connectedToWall03)
+                    if (connectedToWall04 == false)
+                    {
+                        connectedToWall03 = true;
+                    }
+                    else
+                    {
+                        connectedToWall03 = false;
+                        connectedToWall04 = false;
+                    }
+                if (scrIdentifyObjects.connectedToWall04)
+                    if (connectedToWall03 == false)
+                    {
+                        connectedToWall04 = true;
+                    }
+                    else
+                    {
+                        connectedToWall03 = false;
+                        connectedToWall04 = false;
+                    }
+            }
         }
     }
 
 
     private void OnCollisionExit2D(Collision2D collision2D)
     {
-        tagObject = collision2D.gameObject.tag;
-        if (tagObject == "Wall01")
-            connectedToWall01 = false;
-        else if (tagObject == "Wall02")
-            connectedToWall02 = false;
-        else if (tagObject == "Wall03")
-            connectedToWall03 = false;
-        else if (tagObject == "Wall04")
-            connectedToWall04 = false;
-        else if (tagObject == "Player" || tagObject == "Trash")
+        if (ActiveScript == true)
         {
-            scrIdentifyObjects = collision2D.gameObject.GetComponent<IdentifyObjects>();
-
-            if (scrIdentifyObjects.connectedToWall01)
+            tagObject = collision2D.gameObject.tag;
+            if (tagObject == "Wall01")
                 connectedToWall01 = false;
-            if (scrIdentifyObjects.connectedToWall02)
+            else if (tagObject == "Wall02")
                 connectedToWall02 = false;
-            if (scrIdentifyObjects.connectedToWall03)
+            else if (tagObject == "Wall03")
                 connectedToWall03 = false;
-            if (scrIdentifyObjects.connectedToWall04)
+            else if (tagObject == "Wall04")
                 connectedToWall04 = false;
+            else if (tagObject == "Player" || tagObject == "Trash")
+            {
+                scrIdentifyObjects = collision2D.gameObject.GetComponent<IdentifyObjects>();
+
+                if (scrIdentifyObjects.connectedToWall01)
+                    connectedToWall01 = false;
+                if (scrIdentifyObjects.connectedToWall02)
+                    connectedToWall02 = false;
+                if (scrIdentifyObjects.connectedToWall03)
+                    connectedToWall03 = false;
+                if (scrIdentifyObjects.connectedToWall04)
+                    connectedToWall04 = false;
+            }
         }
     }
 }
